8bfb103eb217275005af635f5fac854b023bfa5d
Ludwig88 <luchodd88@gmail.com>
2018-03-28 19:49:43 -0300

    cambio envio para prod

--- trunk/ProcesoPuerto.py
+++ trunk/ProcesoPuerto.py
@@ -6,11 +6,12 @@ from __future__ import print_function
 import os
 dir = os.path.dirname(__file__)
 filename = os.path.join(dir, 'debug/Log.txt')
-log = open(filename, "a+")
+log = None #open(filename, "a+")
 #print("error en el try del serial port",file=log)
 
 from PyQt4 import QtCore
 from PyQt4.Qt import QMutex
+import string
 from collections import deque # double ended queue
 import datetime, time, serial
 
@@ -18,7 +19,8 @@ import datetime, time, serial
 
 class LECTURA(QtCore.QThread):
     def __init__(self, dequePLOT, dequeIN, dequeOUT,
-                 port_num = '/dev/ttyACM0',
+                 #port_num = '/dev/ttyUSB0',
+                 port_num='/dev/ttyACM0',
                  port_baud = 115200,
                  port_stopbits = serial.STOPBITS_ONE,
                  port_parity = serial.PARITY_NONE,
@@ -115,66 +117,68 @@ class LECTURA(QtCore.QThread):
             self.serial_port.close()
 
     def EnviarPS_I(self, ua, celda):
-        print("["+str(datetime.datetime.now())+"][PORT|send] celda a enviar " + str(celda) +" len es: " + str(len(celda)), file=log)
+        print("["+str(datetime.datetime.now())+"][PORT|send] celda a enviar "
+              + str(celda) +" len es: " + str(len(celda)),file=log)
         self.serial_port.write_timeout = 0.3
-        #print "out waiting " + str(self.serial_port.out_waiting)
         """si uso el time out de escritura meter un try catch"""
-        #print "settings " + str(self.serial_port.get_settings())
-        try:
-            #bytes escritos
-            #print(self.serial_port.write(celda), file=log)
-            time.sleep(0.25)
-            #print "["+str(datetime.datetime.now())+"PORT|send] Envio OK"
-            self.serial_port.flushOutput()
-            print("["+str(datetime.datetime.now())+"][PORT|send] "+str(self.serial_port.write(celda)), file=log)
-            self.serial_port.flushOutput()
-            #time.sleep(0.25)
-        except serial.SerialTimeoutException:
-            print("["+str(datetime.datetime.now())+"][PORT|send] timeOut de envio Serie", file=log)
+
         # I=0 con uA=0 en cualquier descarga
         # 2 unidades = 1uA 2048 =0A
-        # global ser
-        # print ser
-        # print 'ua: ' + str(ua) +' ' +str(type(ua)) + 'Descarga: ' + str(Descarga) +' '
-        # + str(type(Descarga)) +'Celda: '  + str(celda) +' ' + str(type(celda))
-        # MatConv=[['h',2],['f',2],['d',2],['b',2],['h',1],
-        #         ['f',1],['d',1],['b',1],['e',2],['g',2],['a',2],
-        #         ['c',2],['e',1],['g',1],['a',1],['c',1]]
+
+        MatConv = [['h', 2], ['f', 2], ['d', 2],
+                   ['b', 2], ['h', 1], ['f', 1],
+                   ['d', 1], ['b', 1], ['e', 2],
+                   ['g', 2], ['a', 2], ['c', 2],
+                   ['e', 1], ['g', 1], ['a', 1], ['c', 1]]
         # cada renglon es el orden en asccii, dentro la primer columna es el caracter dentro del dac cuyo numero es la otra
+
         # letra a enviar MatConv[NumDeCelda(str(celda))][0]
         # numero a enviar MatConv[NumDeCelda(str(celda))][1]
-        # if Descarga :
-        # num=int(2048-(ua*2))
-        # else :
-        # num=int((ua*2)+2048)
-        # num = ua
-        # print 'numero a enviar__: ' +str(num)
-        # if num>=0 and num<=4095:
-        # mil=num/1000
-        # cien=(num-mil*1000)/100
-        # die=(num-mil*1000-cien*100)/10
-        # un=(num-mil*1000-cien*100-die*10)
-        ##print(str(mil)+' '+str(cien)+' '+str(die)+' '+str(un))
-        ##inicio
-        # ser.write('i')
-        ##time.sleep(0.25)
-        # ser.write(str(mil)) #1
-        ##time.sleep(0.25)
-        # ser.write(str(cien)) #2
-        ##time.sleep(0.25)
-        # ser.write(str(die)) #3
-        ##time.sleep(0.25)
-        # ser.write(str(un)) #4
-        ##time.sleep(0.25)
-        # ser.write(str(MatConv[NumDeCelda(str(celda))][0]))
-        ##time.sleep(0.25)
-        # ser.write(str(MatConv[NumDeCelda(str(celda))][1]))
-        ##time.sleep(0.25)
-        # ser.write('f')
-        ##time.sleep(0.25)
-        # ser.flushInput()
-        # else:
-        # print 'numero incorrecto'
+
+        ####################################################################V
+        #si esta invertida la corriente tiene que ser por esto
+        if ua < 0:
+            Descarga = True
+        else:
+            Descarga = False
+
+        if Descarga:
+            num = int(2048 - (abs(ua) * 2))
+        else:
+            num = int((abs(ua) * 2) + 2048)
+
+        print("[" + str(datetime.datetime.now()) + "][PORT|send] abs(ua): " + str(abs(ua))
+              + " " + str(type(ua)) + "Descarga: " + str(Descarga) + " " + str(type(Descarga))
+              + "Celda: " + str(celda) + " " + str(type(celda)))
+
+        if 0 <= num <= 4095:
+            mil = num / 1000
+            cien = (num - mil * 1000) / 100
+            die = (num - mil * 1000 - cien * 100) / 10
+            un = (num - mil * 1000 - cien * 100 - die * 10)
+            try:
+                # inicio
+                self.serial_port.write('i')
+                # time.sleep(0.25)
+                self.serial_port.write(str(mil))  # 1
+                # time.sleep(0.25)
+                self.serial_port.write(str(cien))  # 2
+                # time.sleep(0.25)
+                self.serial_port.write(str(die))  # 3
+                # time.sleep(0.25)
+                self.serial_port.write(str(un))  # 4
+                # time.sleep(0.25)
+                self.serial_port.write(str(MatConv[self.NumDeCelda(celda)][0]))
+                # time.sleep(0.25)
+                self.serial_port.write(str(MatConv[self.NumDeCelda(celda)][1]))
+                # time.sleep(0.25)
+                self.serial_port.write('f')
+                # time.sleep(0.25)
+                self.serial_port.flushInput()
+            except serial.SerialTimeoutException:
+                print("["+str(datetime.datetime.now())+"][PORT|send] timeOut de envio Serie", file=log)
+        else:
+            print("[" + str(datetime.datetime.now()) + "][PORT|send] numero incorrecto", file=log)
 
     def RecibirPS(self):
         s = self.serial_port.readline()
@@ -191,3 +195,8 @@ class LECTURA(QtCore.QThread):
         else:
             return '$#$#$'
 
+    def NumDeCelda(self, celda):
+        for i in range(len(string.ascii_letters)):
+            if celda == string.ascii_letters[i]:
+                return i
+                break
